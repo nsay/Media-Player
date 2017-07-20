6d3967a240cb3669c0152cae94fd02128a3826bf
nsay <23021783+nsay@users.noreply.github.com>
2017-07-20 13:08:19 -0400

    Added 'Open File' option.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,57 @@
 package application;
 	
+import java.io.File;
+import java.net.MalformedURLException;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
-	@Override
+	
+	Player 		player;
+	FileChooser fileChooser;
+		
 	public void start(Stage primaryStage) {
 		
-		Player player = new Player("file:///Users/Narith/Documents/Eclipse%20Projects/Media-Player/assets/sample.mp4");
-		Scene scene = new Scene(player, 640, 400, Color.BLACK);
+		MenuItem open 	= new MenuItem("Open");
+		Menu     file 	= new Menu("File");
+		MenuBar  menu 	= new MenuBar();
+		
+		file.getItems().add(open);
+		menu.getMenus().add(file);
+		
+		fileChooser = new FileChooser();
+		
+		//Set action for open button
+		open.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				player.player.pause();
+				File file = fileChooser.showOpenDialog(primaryStage);
+				if(file != null) {
+					try {
+						player = new Player(file.toURI().toURL().toExternalForm());
+						Scene scene = new Scene(player, 1280, 787, Color.BLACK);
+						primaryStage.setScene(scene);
+					} catch (MalformedURLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}		
+				}
+			}
+		});
+		
+		player = new Player("file://~/Media-Player/assets/sample.mp4");
+		player.setTop(menu);
+		Scene scene = new Scene(player, 1280, 787, Color.BLACK);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
@@ -21,3 +61,4 @@ public class Main extends Application {
 		launch(args);
 	}
 }
+
